02d397d601c7853811ec80d2ae65885b4cd50af0
Dirk Rejahl <dirk@digiglu.co.uk>
2018-03-26 20:52:21 +0100

    wip

--- assets/scripts/services/directory/tmf.js
+++ assets/scripts/services/directory/tmf.js
@@ -4,19 +4,20 @@ export default {
   home: 'https://github.com/SOM-Research/hapi',
   base: 'https://api.github.com/repos/SOM-Research/hapi/git/trees/8176cf7977ca59cab643136fd70632a65aee900b',
   transform (data) {
-    data.tree.sort((a, b) => {
-      return a.path.toLowerCase().localeCompare(b.path.toLowerCase())
-    })
+    const apis = [
+      {
+        'url': 'https://raw.githubusercontent.com/tmforum-apis/TMF669_PartyRole/master/Party_Role_Management.admin.swagger.json',
+        'key': 'TMF669',
+        'title': 'Party Role'
+      },
+      {
+        'url': 'https://raw.githubusercontent.com/tmforum-apis/TMF671_Promotion/master/Promotion_Management.admin.swagger.json',
+        'key': 'TMF671',
+        'title': 'Promotion'
+      }
+    ]
 
-    const apis = []
-
-    for (let i = 0; i < data.tree.length; i++) {
-      apis.push({
-        url: `https://raw.githubusercontent.com/SOM-Research/hapi/master/APIs/generated/mashape/${encodeURIComponent(data.tree[i].path)}`,
-        key: '#' + (i + 1),
-        title: data.tree[i].path.replace(/\.json$/, '')
-      })
-    }
+    console.log(JSON.stringify(apis))
 
     return {apis}
   }
