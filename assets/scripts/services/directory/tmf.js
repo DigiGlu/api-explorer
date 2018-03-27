04690e8600e50d6eceaba8eb561f84fb0437e227
Dirk Rejahl <dirk@digiglu.co.uk>
2018-03-27 11:58:46 +0100

    wip

--- assets/scripts/services/directory/tmf.js
+++ assets/scripts/services/directory/tmf.js
@@ -1,24 +1,10 @@
 export default {
   title: 'TM Forum OpenAPIs',
   subTitle: 'Open APIs published by the TM Forum',
-  home: 'https://github.com/SOM-Research/hapi',
-  base: 'https://api.github.com/repos/SOM-Research/hapi/git/trees/8176cf7977ca59cab643136fd70632a65aee900b',
+  home: 'https://github.com/tmforum-apis',
+  base: 'https://raw.githubusercontent.com/DigiGlu/tmf-api-directory/master/open-api-directory',
   transform (data) {
-    const apis = [
-      {
-        'url': 'https://raw.githubusercontent.com/tmforum-apis/TMF669_PartyRole/master/Party_Role_Management.admin.swagger.json',
-        'key': 'TMF669',
-        'title': 'Party Role'
-      },
-      {
-        'url': 'https://raw.githubusercontent.com/tmforum-apis/TMF671_Promotion/master/Promotion_Management.admin.swagger.json',
-        'key': 'TMF671',
-        'title': 'Promotion'
-      }
-    ]
-
-    console.log(JSON.stringify(apis))
-
+    const apis = data
     return {apis}
   }
 }
