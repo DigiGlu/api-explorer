9531b5f000f77fe77db44e3be920320a80da425d
Dirk Rejahl <dirk@digiglu.co.uk>
2018-05-02 14:16:58 +0100

    wip

---
+++ main/Widget.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  v-content#main
+    router-view
+    #cm-full
+</template>
+
+<script>
+  export { default } from './widget-script'
+</script>
+
+<style lang="stylus">
+  @import '../assets/style/index.styl'
+</style>
